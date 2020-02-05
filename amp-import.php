ff9a7b8c4f9697b7630f2701e0ed85c33b09a22c
kamens503 <mens793@gmail.com>
2020-02-05 12:32:17 -0300

    Add files via upload

---
+++ amp-import.php
@@ -0,0 +1,5 @@
+<!-- iframe for Googlemaps -->
+<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
+<!-- Observer and animation for animations on scroll -->
+<script async custom-element="amp-position-observer" src="https://cdn.ampproject.org/v0/amp-position-observer-0.1.js"></script>
+<script async custom-element="amp-animation" src="https://cdn.ampproject.org/v0/amp-animation-0.1.js"></script>
